0106412255e7a9601ae2b1a2d1d4b6305db29bef
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-09-02 16:40:47 -0500

    login button initializes function

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,31 +1,40 @@
 import React, { Component } from 'react';
 
 class LoginPage extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-  }
+    }
 
-  render() {
-    const { classes } = this.props
-    return (
-      <div>
-          <h1>Test LoginPage</h1>
-          <form onSubmit={this.login}>
-          <div>
-          <h1>Log in here</h1>
-          </div>
-          <label htmlFor="username">
-          </label>
-          <input 
-          type="text"
-          name="username"
-          >
-            </input>
-          </form>
-      </div>
-    );
-  }
+    login = event => {
+        event.preventDefault();
+
+        console.log('init login');
+
+    }
+
+    render() {
+        const { classes } = this.props
+        return (
+            <div>
+                <h1>Test LoginPage</h1>
+                <form onSubmit={this.login}>
+                    <div>
+                        <h1>Log in here</h1>
+                    </div>
+                    <label htmlFor="username">
+                    </label>
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="username"
+                    >
+                    </input>
+                </form>
+                <button onClick={this.login}>login</button>
+            </div>
+        );
+    }
 }
 
 
